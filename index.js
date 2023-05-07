2506a3004bbf2de6542e95f17afdd7534007b7cf
Charly <cascharly@gmail.com>
2023-05-07 17:26:29 -0300

    web server

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
 const http = require("http");
 
+const PORT = 3000;
 const server = http.createServer((req, res) => {
 	res.writeHead(200, {
-		"Content-Type": "text/plain",
+		"Content-Type": "application/json",
 	});
-	res.end();
+	res.end(
+		JSON.stringify({
+			id: 1,
+			name: "Isaac Newton",
+		})
+	);
 });
+
+server.listen(PORT, () => {
+	console.log(`Listening on port ${PORT}`);
+}); // localhost
