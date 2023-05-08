00f458cf66a5daae6da44e0408bb98a662af9fc4
Charly <cascharly@gmail.com>
2023-05-07 22:36:51 -0300

    post data to server

--- index.js
+++ index.js
@@ -21,7 +21,15 @@ const friends = [
 server.on("request", (req, res) => {
 	const items = req.url.split("/");
 	//  /friends/2 => ["","friends","2"]
-	if (items[1] === "friends") {
+	if (req.method === "POST" && items[1] === "friends") {
+		req.on("data", (data) => {
+			const friend = data.toString();
+			console.log("Request: ", friend);
+			friends.push(JSON.parse(friend));
+		});
+	}
+
+	if (req.method === "GET" && items[1] === "friends") {
 		res.statusCode = 200;
 		res.setHeader("Content-Type", "application/json");
 		if (items.length === 3) {
@@ -30,7 +38,7 @@ server.on("request", (req, res) => {
 		} else {
 			res.end(JSON.stringify(friends));
 		}
-	} else if (items[1] === "messages") {
+	} else if (req.method === "GET" && items[1] === "messages") {
 		res.setHeader("Content-Type", "text/html");
 		res.write("<html>");
 		res.write("<body>");
