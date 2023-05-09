7e492a98772bc58d2a41992b796664e17c0ab780
Charly <cascharly@gmail.com>
2023-05-08 21:40:38 -0300

    route parameter

--- server.js
+++ server.js
@@ -4,19 +4,29 @@ const app = express();
 
 const PORT = 3000;
 
-app.get("/", (req, res) => {
-	res.send({
+const friends = [
+	{
+		id: 0,
+		name: "Albert Einstein",
+	},
+	{
 		id: 1,
 		name: "Sir Isaac Newton",
-	});
+	},
+];
+
+app.get("/friends", (req, res) => {
+	res.json(friends);
 });
 
-app.get("/messages", (req, res) => {
-	res.send("<ul><li>Hello Albert</li></ul>");
-});
-
-app.post("/messages", (req, res) => {
-	("Updating messages");
+app.get("/friends/:friendId", (req, res) => {
+	const friendId = Number(req.params.friendId);
+	const friend = friends[friendId];
+	if (friend) {
+		res.status(200).json(friend);
+	} else {
+		res.status(404).json({ error: "Friend does not exist" });
+	}
 });
 
 app.listen(PORT, () => {
